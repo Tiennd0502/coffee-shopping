7377a785b814f17f11385f4c7c588ae40d9500ec
Tien Danh Nguyen <tien.nguyen@asnet.com.vn>
2026-04-17 15:04:32 +0700

    feat: update config connect database

---
+++ coffee-shop-api/src/shared/constants/env.ts
@@ -0,0 +1,6 @@
+export const ENV_ERRORS = {
+  DB_HOST_REQUIRED: 'DB_HOST is required',
+  DB_NAME_REQUIRED: 'DB_NAME is required',
+  DB_USER_REQUIRED: 'DB_USER is required',
+  DB_PASSWORD_REQUIRED: 'DB_PASSWORD is required',
+} as const;
