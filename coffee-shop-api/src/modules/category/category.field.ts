178eafdcb2f332419fe484c9b7c8a8fe8b70ee54
Tien Danh Nguyen <tien.nguyen@asnet.com.vn>
2026-04-21 15:51:58 +0700

    feat: add category update API

--- coffee-shop-api/src/modules/category/category.field.ts
+++ coffee-shop-api/src/modules/category/category.field.ts
@@ -1,6 +1,7 @@
 import type { Category } from '@/modules/category/category.entity';
 
 export const FIELD_KEYS = {
+  ID: 'id',
   NAME: 'name',
   SLUG: 'slug',
 } as const satisfies Partial<Record<string, keyof Category>>;
